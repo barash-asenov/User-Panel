abd361fda1595fa5b48684de01d6a601d2115c1e
Barash Hristov Asenov <barash.asenov@gmail.com>
2019-12-18 20:26:57 +0300

    Counter has been implemented

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,23 +2,25 @@
 	<h1>Users</h1>
 	<br/>
 	<?php if (isset($users) && !empty($users)): ?>
-	<table class="table">
-		<thead>
-		<tr>
-			<th scope="col">#</th>
-			<th scope="col">Username</th>
-			<th scope="col">Email</th>
-		</tr>
-		</thead>
-		<tbody>
+		<table class="table">
+			<thead>
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Username</th>
+				<th scope="col">Email</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php $counter = 1; ?>
 			<?php foreach ($users as $user): ?>
 				<tr>
-					<th scope="row">1</th>
+					<th scope="row"><?= $counter ?></th>
 					<td><?= $user['username'] ?></td>
 					<td><?= $user['email'] ?></td>
 				</tr>
+				<?php $counter++; ?>
 			<?php endforeach; ?>
-	</table>
+		</table>
 	<?php else: ?>
 		<div class="alert alert-danger" role="alert">
 			No User Found!
